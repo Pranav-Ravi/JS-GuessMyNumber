84c8131334130b29679a8b2a3a37eb22c11ae0ee
Pranav Ravi <57525557+Pranav-Ravi@users.noreply.github.com>
2020-10-29 22:57:24 +0530

    DRY princicple aaplied, added again button events
    DRY princicple aaplied, added again button events
    * create a custom function to pass in any message to the messagebox, since it was used often
    * Again button now resets all the values before starting a new game

--- app.js
+++ app.js
@@ -5,47 +5,61 @@
 // document.querySelector('.number').textContent = 13;
 // document.querySelector('.guess').value = 23;
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
-document.querySelector('.number').textContent = secretNumber;
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
 
     //when there is no input
     if(!guess) {
-        document.querySelector('.message').textContent = "Enter Guessed Number";
+        displayMessage("No number!");
 
     //when the player wins
     } else if(guess === secretNumber) {
-        document.querySelector('.message').textContent = "Correct Answer!";
-        highScore++;
-        document.querySelector('.highscore').textContent = highScore;
+        displayMessage("Correct Answer!");
+        document.querySelector('.number').textContent = secretNumber;
+        if(highScore < score) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+        
         //visual changes
         document.querySelector('body').style.background = '#57cc99';
         document.querySelector('.number').style.width = '30rem';
-
-    //when guess is too high
-    } else if(guess > secretNumber) {
-        if(score > 0) {
-            document.querySelector('.message').textContent = "Too high!";
+    
+    //when guessed number is not equal to secretNumber
+    } else if(guess !== secretNumber) {
+        if(score > 1) {
+            displayMessage(guess > secretNumber? 
+            "Too High!" : "Too Low");
             score--;
             document.querySelector('.score').textContent = score;
         } else {
-            document.querySelector('.message').textContent = "You lost!";
+            displayMessage("You lost!");
             document.querySelector('.score').textContent = 0;
-        }  
-
-    //when guess is too low
-    } else if(guess < secretNumber) {
-        if(score > 0) {
-            document.querySelector('.message').textContent = "Too low!";
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = "You lost!";
-            document.querySelector('.score').textContent = 0;
-        }   
+        }
     }
+});
+
+document.querySelector('.again').addEventListener('click', function() {
+
+    //store default values for score and other numbers
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+    //restore the default values of message, number, score and input field
+    document.querySelector('.score').textContent = score;
+    displayMessage("start guessing!");
+    document.querySelector('.number').textContent = "?";
+    document.querySelector('.guess').value = '';
+
+
+    //restore background color and number width
+    document.querySelector('body').style.background = '#222';
+    document.querySelector('.number').style.width = '15rem';
 });
